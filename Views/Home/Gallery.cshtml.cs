5e3a53449007b1302a0c869c03311095b208e5c7
Lytvyniuk Maxym <max.litvinyuck553@gmail.com>
2022-02-21 21:02:02 +0200

    2/21/22 9pm Gallery items, strings

--- Views/Home/Gallery.cshtml.cs
+++ Views/Home/Gallery.cshtml.cs
@@ -1,15 +1,16 @@
 ﻿using Microsoft.AspNetCore.Mvc.RazorPages;
 using MVC_app_main.Models;
 using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 using System.Net.Http.Headers;
 
 namespace MVC_app_main.Views.Home
 {
     public class GalleryModel : PageModel
     {
-        private NASAImages? Images { get; set; }
-
-        public async Task<NASAImages> GetPhotos()
+        //private ItemsSon? Images { get; set; }
+        IList<ItemsSon> nasaImages = new List<ItemsSon>();
+        public async Task<IList<ItemsSon>> GetPhotos()
         {
             using var client = new HttpClient();
             client.BaseAddress = new Uri("https://images-api.nasa.gov/search?year_start=2022");
@@ -19,11 +20,21 @@ namespace MVC_app_main.Views.Home
             if (response.IsSuccessStatusCode)
             {
                 string res = await response.Content.ReadAsStringAsync();
-                res = res[res.IndexOf('[')..];
-                res = res.Remove(res.IndexOf("metadata") - 2);//перевірити
-                Images = JsonConvert.DeserializeObject<NASAImages>(res);
+                //res = res[res.IndexOf('[')..];
+                //res = res.Remove(res.IndexOf("metadata") - 3);//перевірити
+                //Images = JsonConvert.DeserializeObject<NASAImages>(res);
+                JObject result = JObject.Parse(res);
+                IList<JToken> items = result["collection"]["items"].Children().ToList();
+
+                
+
+                foreach (JToken item in items)
+                {
+                    ItemsSon n = item.ToObject<ItemsSon>();
+                    nasaImages.Add(n);
+                }
             }
-            return Images;
+            return nasaImages;
         }
     }
 }
